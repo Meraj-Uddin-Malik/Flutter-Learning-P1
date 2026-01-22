ac9e2fb9d24a370527e4d5050ba26bf2b5cba42e
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-23 02:07:59 +0500

    update mi_card layout to Row with nested Column and new colors

--- lib/mi_card.dart
+++ lib/mi_card.dart
@@ -7,32 +7,23 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal,
-      body: SafeArea(child:
-      Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 200.0,
-            width: double.infinity,
-            color: Colors.amber,
-            child: Text("Hello",style: TextStyle(color: Colors.white, fontSize: 19.0),),
-          ),
-          Container(
-            height: 200.0,
-            width: double.infinity,
-            color: Colors.brown,
-            child: Text("Hello",style: TextStyle(color: Colors.white, fontSize: 19.0),),
-          ),
-          Container(
-            height: 200.0,
-            width: double.infinity,
-            color: Colors.blueAccent,
-            child: Text("Hello",style: TextStyle(color: Colors.white, fontSize: 19.0),),
-          ),
-        ],
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(width: 70.6, color: Colors.red),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(width: 100.0, height: 100.0, color: Colors.amber),
+                Container(width: 100.0, height: 100.0, color: Colors.green),
+              ],
+            ),
+            Container(width: 60.0, color: Colors.blueAccent),
+          ],
+        ),
       ),
-      )
     );
   }
 }
