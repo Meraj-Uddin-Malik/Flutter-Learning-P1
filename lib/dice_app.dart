dd5eda8e4e0449378802b1c2ed2eb539dfdec882
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-24 20:36:53 +0500

    add DiceApp and update main home widget

---
+++ lib/dice_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DiceApp extends StatefulWidget {
+  const DiceApp({super.key});
+
+  @override
+  State<DiceApp> createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learning_flutter/mi_card.dart';
+import 'package:learning_flutter/dice_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: const MiCard(),
+      home: const DiceApp(),
     );
   }
 }
